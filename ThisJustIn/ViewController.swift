afa818b2550aff9fa902c824aede5bccdea6ec73
Eymen Varilci <evarilci@gmail.com>
2023-07-13 21:24:53 +0300

    Initial Commit
    Foldering files and getting rid of storyBoard done. Ready to start.

---
+++ ThisJustIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThisJustIn
+//
+//  Created by Eymen Varilci on 13.07.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
